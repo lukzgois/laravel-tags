858dd3e89b796adb4c30a35cefcadbcdb1688dc2
Lukz <lucaspgois@gmail.com>
2019-05-13 09:28:42 -0300

    WIP: Adding custom tag class config

---
+++ tests/CustomTagTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Spatie\Translatable\Test;
+
+use Spatie\Tags\Tag;
+use Spatie\Tags\Test\TestCase;
+
+class CustomTagTest extends TestCase {
+
+
+    public function it_checks_if_the_custom_tag_extends_the_base_class()
+    {
+
+    }
+}
